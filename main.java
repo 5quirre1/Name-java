d8bdd65596e8f48f63c6c04b00239f029b2a02d3
Squirrel <squirrelhomebrew@gmail.com>
2025-01-10 11:55:04 -0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    String userName;
+    
+    System.out.println("what is your name vro?"); 
+    userName = myObj.nextLine();   
+       
+    System.out.println("your nmae is: " + userName);   
+    System.out.println("this pained me to code this sob...");
+  } 
+}
